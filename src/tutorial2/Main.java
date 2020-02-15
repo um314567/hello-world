e1d1b21e7a42f0d548b434c507a36d24d3ed93ad
um314 <um314@DESKTOP-LI6V9EF>
2020-02-16 02:35:55 +0900

    initial commit

---
+++ src/tutorial2/Main.java
@@ -0,0 +1,14 @@
+package tutorial2;
+
+public class Main {
+	{
+		System.out.println(2 << 1);
+		System.out.println(2 << 2);
+		System.out.println(2 << 3);
+		System.out.println(8 >> 1);
+		System.out.println(8 >> 2);
+		System.out.println(8 >> 3);
+	}
+	
+	}
+
